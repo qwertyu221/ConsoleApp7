44c34b57c2fcf852c67d740eb70dde460216a714
Nikita <e4132dm@gmail.com>
2022-03-19 02:39:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/ParseProduct.cs
@@ -0,0 +1,62 @@
+﻿using AngleSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class ParseProduct
+    {
+        public async Task ParseProd() {
+
+            
+
+
+            await TakeInfoAsync("h1.detail-name");
+
+            await TakeInfoAsync("span.price");
+
+            await TakeInfoAsync("span.ok");
+
+            
+           
+
+
+          
+
+            //var titles = cells.Select(m => m.TextContent);
+            //Console.WriteLine(titles.ToString());
+
+        }
+
+        public void AddToList (ref List<String> listOfPage, ref String cells) {
+
+            listOfPage.Add(cells);
+        }
+
+        public async Task TakeInfoAsync (string cellSelector) {
+
+            List<String> listOfPage = new List<string>();
+            var config = Configuration.Default.WithDefaultLoader();   //парсинг имени
+            var address = "https://www.toy.ru/catalog/mashinki_iz_multfilmov/fortnite_fnt0163_mashina_quadcrasher/";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+
+            var cells = document.QuerySelector(cellSelector).TextContent;
+            AddToList(ref listOfPage, ref cells);
+
+
+            foreach (var i in listOfPage) {
+                Console.WriteLine(i);
+            }
+        }
+
+
+    }
+
+        
+}
+
