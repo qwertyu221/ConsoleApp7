ef8daa253f2893563da7cad6683b366efe556da3
Nikita <e4132dm@gmail.com>
2022-03-19 16:40:02 +0300

    сделано: взятие части данных из товара

--- ConsoleApp7/ParseProduct.cs
+++ ConsoleApp7/ParseProduct.cs
@@ -1,4 +1,5 @@
 ﻿using AngleSharp;
+using AngleSharp.Dom;
 
 using System;
 using System.Collections.Generic;
@@ -12,47 +13,59 @@ namespace ConsoleApp7
     {
         public async Task ParseProd() {
 
-            
+            List<IElement> listOfPage = new List<IElement>();
 
 
+            await TakeInfoAsync("nav.breadcrumb");
             await TakeInfoAsync("h1.detail-name");
-
+            await TakeInfoAsync("span.old-price");
             await TakeInfoAsync("span.price");
-
             await TakeInfoAsync("span.ok");
-
-            
-           
-
-
-          
-
-            //var titles = cells.Select(m => m.TextContent);
-            //Console.WriteLine(titles.ToString());
-
-        }
-
-        public void AddToList (ref List<String> listOfPage, ref String cells) {
-
-            listOfPage.Add(cells);
-        }
-
-        public async Task TakeInfoAsync (string cellSelector) {
-
-            List<String> listOfPage = new List<string>();
-            var config = Configuration.Default.WithDefaultLoader();   //парсинг имени
-            var address = "https://www.toy.ru/catalog/mashinki_iz_multfilmov/fortnite_fnt0163_mashina_quadcrasher/";
-            var context = BrowsingContext.New(config);
-            var document = await context.OpenAsync(address);
-
-            var cells = document.QuerySelector(cellSelector).TextContent;
-            AddToList(ref listOfPage, ref cells);
+            await TakeInfoAsync("div.col-12.select-city-link");
+            await TakeInfoAsync("div.col-12.col-md-10.col-lg-7");
 
 
             foreach (var i in listOfPage) {
-                Console.WriteLine(i);
+                Console.WriteLine(i.TextContent.Trim());
             }
+
+
+
+            void AddToList (ref List<IElement> listOfPage, ref IElement cells) {
+
+                listOfPage.Add(cells);
+            }
+
+            async Task TakeInfoAsync (string cellSelector) {
+
+
+                var config = Configuration.Default.WithDefaultLoader();   //парсинг имени
+                var address = "https://www.toy.ru/catalog/mashinki_iz_multfilmov/fortnite_fnt0163_mashina_quadcrasher/";
+                var context = BrowsingContext.New(config);
+                var document = await context.OpenAsync(address);
+
+
+
+                var cells = document.QuerySelector(cellSelector);
+
+                if (cells != null) {
+                    AddToList(ref listOfPage, ref cells);
+                }
+
+
+                
+
+
+               
+            }
+
+
+           
+
+
         }
+
+       
 
 
     }
--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -10,54 +10,11 @@ namespace ConsoleApp7
 {
     class Program
     {
-        async static Task Main (string[] args) {
+        public static async Task Main (string[] args) {
 
-            List<String> listOfPage = new List<string>();
+            ParseProduct parse = new ParseProduct();
 
-            //var config = Configuration.Default.WithDefaultLoader();
-            //var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
-            //var context = BrowsingContext.New(config);
-            //var document = await context.OpenAsync(address);
-            //var cellSelector = "td:nth-child(3)";
-            //var cells = document.QuerySelectorAll(cellSelector);
-            //var titles = cells.Select(m => m.TextContent);
-
-
-
-            //foreach (var i in titles) {
-            //    Console.WriteLine(i.ToString());
-            //}
-
-            await TakeInfoAsync("h1.detail-name");
-            await TakeInfoAsync("span.price");
-            await TakeInfoAsync("span.ok");
-
-            foreach (var i in listOfPage) {
-                Console.WriteLine(i);
-            }
-
-
-            void AddToList (ref List<String> listOfPage, ref String cells) {
-
-                listOfPage.Add(cells);
-            }
-
-            async Task TakeInfoAsync (string cellSelector) {
-
-               
-                var config = Configuration.Default.WithDefaultLoader();   //парсинг имени
-                var address = "https://www.toy.ru/catalog/mashinki_iz_multfilmov/fortnite_fnt0163_mashina_quadcrasher/";
-                var context = BrowsingContext.New(config);
-                var document = await context.OpenAsync(address);
-
-                var cells = document.QuerySelector(cellSelector).TextContent;
-                AddToList(ref listOfPage, ref cells);
-
-
-                //foreach (var i in listOfPage) {
-                //    Console.WriteLine(i);
-                //}
-            }
+            await parse.ParseProd();
         }
     }
 }
